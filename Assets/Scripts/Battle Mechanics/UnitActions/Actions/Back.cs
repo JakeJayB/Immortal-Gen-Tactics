c6088cdfb55493b992aafab7628712275da1a3d5
AndrewCIP <a_lewis18@u.pacific.edu>
2025-03-16 09:26:44 -0700

    Back MenuSlotImage Updated

--- Assets/Scripts/Battle Mechanics/UnitActions/Actions/Back.cs
+++ Assets/Scripts/Battle Mechanics/UnitActions/Actions/Back.cs
@@ -4,23 +4,11 @@ using UnityEngine;
 
 public class Back : UnitAction
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public override string Name { get; protected set; } = "Back";
-    public override int APCost { get; protected set; } = 0;
-    public override int Priority { get; protected set; } = 0;
+    public override int APCost { get; protected set; }
+    public override int Priority { get; protected set; }
     public override ActionType ActionType { get; protected set; }
-    public override string SlotImageAddress { get; protected set; } = "Sprites/UnitMenu/Slots/igt_item";
+    public override string SlotImageAddress { get; protected set; } = "Sprites/UnitMenu/Slots/igt_back";
     
     public override Sprite SlotImage() { return Resources.Load<Sprite>(SlotImageAddress); }
 
