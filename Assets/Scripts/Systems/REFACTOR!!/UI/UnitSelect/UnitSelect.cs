4211c143d1a4c4474a52d340fd701febf3038b2c
AndrewCIP <a_lewis18@u.pacific.edu>
2025-12-30 21:17:23 -0800

    UnitSelect Entry Transition Added

--- Assets/Scripts/Systems/REFACTOR!!/UI/UnitSelect/UnitSelect.cs
+++ Assets/Scripts/Systems/REFACTOR!!/UI/UnitSelect/UnitSelect.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Graphs;
 using UnityEngine;
 
 namespace IGT.Systems
@@ -8,10 +9,14 @@ namespace IGT.Systems
     {
         private const int UNIT_SELECT_SIZE = 15;
         private const int UNIT_SELECT_ROW_COUNT = 3;
+        private const float START_TRANSITION_OFFSET = 1200f;
 
         private static GameObject gameObj;
         private int index;
         [SerializeField] private UnitSelectSlot[] UnitSelectSlots = new UnitSelectSlot[UNIT_SELECT_SIZE];
+        [SerializeField] private Vector2[] SlotPositions = new Vector2[UNIT_SELECT_SIZE];
+        private static bool isActivated = false;
+        private bool canInteract = false;
 
         private void Awake()
         {
@@ -28,21 +33,39 @@ namespace IGT.Systems
         // Update is called once per frame
         void Update()
         {
-            if (!Input.anyKeyDown) { return; }
+            if (isActivated) {
+                isActivated = false;
+                StartCoroutine(PerformEntryTransition());
+                return;
+            }
 
-            MoveUp();
-            MoveDown();
-            MoveLeft();
-            MoveRight();
-            SelectUnitSlot();
-            ExitUnitSelect();
+            if (canInteract)
+            {
+                if (!Input.anyKeyDown) { return; }
+
+                MoveUp();
+                MoveDown();
+                MoveLeft();
+                MoveRight();
+                SelectUnitSlot();
+                ExitUnitSelect();
+            }
         }
 
         private void InitializeUnitSelect()
         {
+            RecordUnitSlotPositions();
             AssignUnitSlots();
             index = 7;
             UnitSelectSlots[index].HighlightSlot();
+        }
+
+        private void RecordUnitSlotPositions()
+        {
+            for (int i = 0; i < UNIT_SELECT_SIZE; i++)
+            {
+                SlotPositions[i] = UnitSelectSlots[i].GetComponent<RectTransform>().anchoredPosition;
+            }
         }
 
         private void AssignUnitSlots()
@@ -52,6 +75,60 @@ namespace IGT.Systems
                 UnitSelectSlots[i].ReferenceUnit(PartyManager.unitList[i]);
                 UnitSelectSlots[i].UpdatePortrait();
             }
+        }
+
+        private IEnumerator PerformEntryTransition()
+        {
+            float slotDelay = 0.025f;
+
+            for (int col = 0; col < 5; col++)
+            {
+                for (int row = 0; row < 3; row++)
+                {
+                    int slotNum = (row * 5) + col;
+                    RectTransform slot = UnitSelectSlots[slotNum].GetComponent<RectTransform>();
+                    Vector2 target = SlotPositions[slotNum];
+
+                    if (col == 4 && row == 2)
+                    {
+                        yield return MoveSlot(slot, target);
+                    }
+                    else
+                    {
+                        StartCoroutine(MoveSlot(slot, target));
+                        yield return new WaitForSeconds(slotDelay);
+                    }          
+                }
+            }
+            canInteract = true;
+        }
+
+        private void OffsetSlots()
+        {
+            foreach (UnitSelectSlot slot in UnitSelectSlots){
+                slot.GetComponent<RectTransform>().anchoredPosition += new Vector2(START_TRANSITION_OFFSET, 0);
+            }
+        }
+
+        IEnumerator MoveSlot(RectTransform slot, Vector2 end)
+        {
+            float moveDuration = 0.25f;
+            Vector2 start = slot.anchoredPosition;
+            float elapsed = 0f;
+
+            while (elapsed < moveDuration)
+            {
+                elapsed += Time.deltaTime;
+                float t = elapsed / moveDuration;
+
+                // Smooth easing
+                t = Mathf.SmoothStep(0f, 1f, t);
+
+                slot.anchoredPosition = Vector2.Lerp(start, end, t);
+                yield return null;
+            }
+
+            slot.anchoredPosition = end;
         }
 
         private void MoveUp()
@@ -144,7 +221,14 @@ namespace IGT.Systems
             }
         }
 
-        public static void SetGameObjActive() { gameObj.SetActive(true); }
-        public static void SetGameObjInactive() { gameObj.SetActive(false); }
+        public static void SetGameObjActive() { 
+            gameObj.SetActive(true);
+            isActivated = true;
+        }
+        public void SetGameObjInactive() {
+            OffsetSlots();
+            canInteract = false;
+            gameObj.SetActive(false); 
+        }
     }
 }
